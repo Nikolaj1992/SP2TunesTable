b77fd1dca22c6ac08ac5ef0423491261c8835000
Loudi <loudimail@gmail.com>
2024-10-22 12:34:09 +0200

    dto setup

--- src/main/java/app/entities/Song.java
+++ src/main/java/app/entities/Song.java
@@ -1,4 +1,21 @@
 package app.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Song {
+    @JsonProperty("name")
+    String name;
+    @JsonProperty("type")
+    String type;
+    //not fetching artist, adding the primary artist manually
+    @JsonProperty("track_number")
+    int songNumber;
 }
