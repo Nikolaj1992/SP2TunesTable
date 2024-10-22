e2d704bfaf63b01966bcf2ea4b37292b8d794e95
Loudi <loudimail@gmail.com>
2024-10-22 14:52:30 +0200

    in dire need of a dao, so i can create the album id

--- src/main/java/app/entities/Album.java
+++ src/main/java/app/entities/Album.java
@@ -1,4 +1,61 @@
 package app.entities;
 
+import app.dtos.AlbumDTO;
+import app.dtos.ArtistDTO;
+import app.dtos.SongDTO;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Album {
+    @Id
+    private String id; //is given right after Artist is persisted
+    private String name;
+    private String type;
+    private int totalSongs;
+    private String releaseDate;
+    @ManyToOne
+    private Artist artist;
+    @OneToMany
+    private List<Song> songs = new ArrayList<>();
+
+    public Album(AlbumDTO dto) {
+        this.name = dto.getName();
+        this.type = dto.getType();
+        this.totalSongs = dto.getTotalSongs();
+        addSongs(dto.getTracks().getSongs());
+        addReleaseDate(dto.getReleaseDate(), dto.getReleaseDatePrecision());
+    }
+
+    private void addSongs(List<SongDTO> songs) {
+        if (!this.songs.isEmpty() && !songs.isEmpty()) {
+            for (SongDTO dto : songs) {
+                Song song = new Song(dto);
+                String id = this.id + "-" + song.getSongNumber();
+                song.setId(id);
+                song.setAlbum(this);
+                this.songs.add(song);
+            }
+        }
+    }
+
+    private void addReleaseDate(String rd, String rdp) {
+        if (this.releaseDate != null && rd != "" && rdp == "day") {
+            this.releaseDate = rd;
+        }
+        //rd = release date
+        //rdp = release date precision
+    }
+
 }
--- src/main/java/app/entities/Artist.java
+++ src/main/java/app/entities/Artist.java
@@ -1,4 +1,36 @@
 package app.entities;
 
+import app.dtos.ArtistDTO;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Artist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String name;
+    private String type;
+    @OneToMany(mappedBy = "id")
+    private List<Album> albums = new ArrayList<>();
+
+    public Artist(ArtistDTO dto){
+        this.name = dto.getName();
+        this.type = dto.getType();
+    }
+
+    public void addAlbum(Album album){
+        if (!albums.contains(album)){
+            album.setArtist(this);
+            this.albums.add(album);
+        }
+    }
 }
