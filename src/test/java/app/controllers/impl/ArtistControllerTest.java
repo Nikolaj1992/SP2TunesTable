bcc56835ec81607b73d6a0061ea9837ada97d59b
NikolajF <cph-nf70@cphbusiness.dk>
2024-10-23 13:32:22 +0200

    ArtistControllerTest set up. Only create and delete fails. Seems to be a problem related to post

--- src/test/java/app/controllers/impl/ArtistControllerTest.java
+++ src/test/java/app/controllers/impl/ArtistControllerTest.java
@@ -1,57 +1,169 @@
 package app.controllers.impl;
 
+import app.config.ApplicationConfig;
+import app.config.HibernateConfig;
+import app.dtos.ArtistDTO;
+import app.entities.Artist;
 import io.javalin.Javalin;
+import io.restassured.RestAssured;
 import jakarta.persistence.EntityManagerFactory;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 class ArtistControllerTest {
 
-    private static Javalin app;
     private static EntityManagerFactory emf;
+    private static Javalin app;
+    private static final int TEST_PORT = 7000;
+
+    private ArtistDTO artist1;
+    private ArtistDTO artist2;
 
     @BeforeAll
-    public static void setUp() {
-//        System.setProperty("hibernate.connection.url", postgres.getJdbcUrl());
-//        System.setProperty("hibernate.connection.username", postgres.getUsername());
-//        System.setProperty("hibernate.connection.password", postgres.getPassword());
-//
-//        HibernateConfig.setTest(true);
-//        emf = HibernateConfig.getEntityManagerFactoryForTest();
-//
-//        app = Javalin.create().start(7000);
-//        app.routes(() -> {
-//            AlbumController albumController = new AlbumController();
-//            app.get("/albums", albumController::readAll);
-//            app.get("/albums/:id", albumController::read);
-//            app.post("/albums", albumController::create);
-//            app.put("/albums/:id", albumController::update);
-//            app.delete("/albums/:id", albumController::delete);
-//        });
-//
-//        RestAssured.baseURI = "http://localhost:7000";
+    static void setUpAll() {
+        HibernateConfig.setTest(true);
+        emf = HibernateConfig.getEntityManagerFactoryForTest();
+        app = ApplicationConfig.startServer(TEST_PORT);
+
+        RestAssured.baseURI = "http://localhost:" + TEST_PORT + "/api";
     }
+
+    @AfterAll
+    static void tearDownAll() {
+        ApplicationConfig.stopServer(app);
+        emf.close();
+    }
+
+    @BeforeEach
+    void setUp() {
+        artist1 = new ArtistDTO();
+        artist1.setName("Test Artist 1");
+        artist1.setType("artist");
+
+        artist2 = new ArtistDTO();
+        artist2.setName("Test Artist 2");
+        artist2.setType("artist");
+
+        Artist entity1 = new Artist(artist1);
+        Artist entity2 = new Artist(artist2);
+
+        try (var em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            em.createQuery("DELETE FROM Artist").executeUpdate();
+            em.persist(entity1);
+            artist1.setId(String.valueOf(entity1.getId()));
+            em.persist(entity2);
+            artist2.setId(String.valueOf(entity2.getId()));
+            em.getTransaction().commit();
+        }
+    }
+
+    // error from post possibly not working
+//    @BeforeEach
+//    void setUp() {
+//        clearArtists();
+//        createArtist("Test Artist 1");
+//        createArtist("Test Artist 2");
+//    }
+//
+//    // Helper methods
+//    private void clearArtists() {
+//        given()
+//                .when()
+//                .delete("/artists")
+//                .then()
+//                .statusCode(204);
+//    }
+//
+//    private void createArtist(String name) {
+//        given()
+//                .contentType("application/json")
+//                .body("{\"name\":\"" + name + "\", \"type\":\"artist\"}")
+//                .when()
+//                .post("/artists")
+//                .then()
+//                .statusCode(201);
+//    }
 
     @Test
     void read() {
+        given()
+                .pathParam("id", 1) // Assuming ID of created artist is 1
+                .when()
+                .get("/artists/{id}")
+                .then()
+                .statusCode(200)
+                .body("name", equalTo("Test Artist 1"));
     }
 
     @Test
     void readAll() {
+        given()
+                .when()
+                .get("/artists")
+                .then()
+                .statusCode(200)
+                .body("size()", equalTo(2)); // Expecting two artists created in setup
     }
 
     @Test
     void create() {
+        given()
+                .contentType("application/json")
+                .body("{\"name\":\"Test Artist 3\", \"type\":\"artist\"}")
+                .when()
+                .post("/artists")
+                .then()
+                .statusCode(201);
+
+        given()
+                .when()
+                .get("/artists")
+                .then()
+                .statusCode(200)
+                .body("size()", equalTo(3)); // Expecting three artists now
     }
 
     @Test
     void update() {
+        given()
+                .pathParam("id", 1) // Assuming ID of created artist is 1
+                .contentType("application/json")
+                .body("{\"name\":\"Updated Artist\", \"type\":\"artist\"}")
+                .when()
+                .put("/artists/{id}")
+                .then()
+                .statusCode(200);
+
+        given()
+                .pathParam("id", 1)
+                .when()
+                .get("/artists/{id}")
+                .then()
+                .statusCode(200)
+                .body("name", equalTo("Updated Artist"));
     }
 
     @Test
     void delete() {
+        given()
+                .pathParam("id", 1) // Assuming ID of created artist is 1
+                .when()
+                .delete("/artists/{id}")
+                .then()
+                .statusCode(204);
+
+        given()
+                .pathParam("id", 1)
+                .when()
+                .get("/artists/{id}")
+                .then()
+                .statusCode(404);
     }
 
 }
