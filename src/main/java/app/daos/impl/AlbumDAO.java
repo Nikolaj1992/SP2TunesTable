215cf194d3dfa994151630acad6b91bebd18dcf7
NikolajF <cph-nf70@cphbusiness.dk>
2024-10-27 14:51:07 +0100

    For deployment

--- src/main/java/app/daos/impl/AlbumDAO.java
+++ src/main/java/app/daos/impl/AlbumDAO.java
@@ -1,9 +1,15 @@
 package app.daos.impl;
 
+import app.config.HibernateConfig;
 import app.daos.IDAO;
 import app.dtos.AlbumDTO;
 import app.entities.Album;
+import app.entities.Artist;
 import app.exceptions.DaoException;
+import app.security.daos.SecurityDAO;
+import app.security.enums.Role;
+import app.utils.Utils;
+import app.utils.json.JsonReader;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.TypedQuery;
@@ -101,4 +107,57 @@ public class AlbumDAO implements IDAO<AlbumDTO, Integer> {
             return album != null;
         }
     }
+
+    public void populate(){
+        // Environment state and credentials for user/admin handled in config.properties via Utils class
+        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
+        SecurityDAO securityDAO = new SecurityDAO(emf);     // securityDao handles creation of users and roles
+
+        List<AlbumDTO> albumDTOs = JsonReader.readAlbums("");
+
+        try (var em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            createRoles(securityDAO);   // creates roles in DB if they do not exist
+
+            em.getTransaction().commit();
+
+            int availableAlbumIndex = 1; //starts at one because place 0 is for singles
+            for (AlbumDTO albumDTO : albumDTOs) {
+                em.getTransaction().begin();
+
+                Artist artist = new Artist(albumDTO.getArtists().get(0));
+                em.persist(artist);
+
+                em.flush();     // Flush to get the artist ID
+
+                int existingAlbums = em.createQuery(
+                                "SELECT COUNT(a) FROM Album a WHERE a.artist.id = :artistId", Long.class)
+                        .setParameter("artistId", artist.getId())   // Artist should have ID from persist above
+                        .getSingleResult()
+                        .intValue();
+                availableAlbumIndex = availableAlbumIndex + existingAlbums;
+
+                artist.addAlbumAsDTO(albumDTO, availableAlbumIndex);
+                em.persist(artist);
+
+                em.getTransaction().commit();
+            }
+
+            System.out.println("Albums added to database");
+        } catch (Exception e) {
+            System.out.println("Failed to add albums to database: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private void createRoles(SecurityDAO securityDAO) {
+        try {
+            securityDAO.createRoleIfNotPresent(Role.ANYONE.name());
+            securityDAO.createRoleIfNotPresent(Role.USER.name());
+            securityDAO.createRoleIfNotPresent(Role.ADMIN.name());
+        } catch (Exception e) {
+            System.out.println("Failed to create roles: " + e.getMessage());
+        }
+    }
+
 }
