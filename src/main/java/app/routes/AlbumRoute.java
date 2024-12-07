005877cee891cb6bd0b6db7221b2c7c84d4619b3
NikolajF <cph-nf70@cphbusiness.dk>
2024-12-07 13:15:37 +0100

    CORS headers added to ApplicationConfig. Changes to dev and AlbumRoute, were purely for testing before security gets added.

--- src/main/java/app/routes/AlbumRoute.java
+++ src/main/java/app/routes/AlbumRoute.java
@@ -15,7 +15,7 @@ public class AlbumRoute {
         return () -> {
             post("/", albumController::create, Role.ADMIN);
             get("/", albumController::readAll, Role.ANYONE, Role.USER);
-            get("/{id}", albumController::read, Role.ANYONE, Role.USER);
+            get("/{id}", albumController::read, Role.USER); // re-add Role.ANYONE later
             put("/{id}", albumController::update, Role.ADMIN);
             delete("/{id}", albumController::delete, Role.ADMIN); //make this turn all songs into singles or add a word to indicate they are former album songs
         };
